7872327994223d00dc001ab8674f526af3de1d87
Gabrielhlc <gabrielhenlc@gmail.com>
2023-04-25 14:06:01 -0300

    add: my ad

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,10 @@
+import { TouchableOpacity } from "react-native";
 import { HStack, ScrollView, Text, VStack } from "native-base";
 
 import { Avatar } from "@components/Avatar";
 import { Button } from "@components/Button";
+
+import { ArrowRight, Tag } from "phosphor-react-native";
 
 export function Home() {
     return (
@@ -27,6 +30,62 @@ export function Home() {
                         variant="black"
                     />
                 </HStack>
+
+                <VStack mt={8} space={3}>
+                    <Text
+                        fontSize="sm"
+                        color="gray.300"
+                    >
+                        Seus produtos anunciados para venda
+                    </Text>
+
+                    <HStack
+                        w="full"
+                        h={66}
+                        py={3}
+                        pr={5}
+                        pl={4}
+                        alignItems="center"
+                        justifyContent="space-between"
+                        background="rgba(var(--blue-light), 0.1)"
+                        borderRadius={6}
+                    >
+                        <HStack alignItems="center" space={4}>
+                            <Tag size={22} color="#364D9D" />
+
+                            <VStack>
+                                <Text
+                                    fontWeight="bold"
+                                    fontSize="lg"
+                                    color="gray.200"
+                                >
+                                    4
+                                </Text>
+
+                                <Text
+                                    fontSize="xs"
+                                    color="gray.200"
+                                >
+                                    anúncios ativos
+                                </Text>
+                            </VStack>
+                        </HStack>
+
+                        <TouchableOpacity>
+                            <HStack alignItems="center" space={2}>
+                                <Text
+                                    color="blue"
+                                    fontSize="xs"
+                                    fontWeight="bold"
+                                >
+                                    Meus anúncios
+                                </Text>
+
+                                <ArrowRight size={16} color="#364D9D" />
+                            </HStack>
+                        </TouchableOpacity>
+                    </HStack>
+                </VStack>
             </VStack>
 
         </ScrollView>
